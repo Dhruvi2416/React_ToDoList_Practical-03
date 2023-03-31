9f756abd06774e1994af5183eec9d99099c8fd0c
Dhruvi2416 <dhruvi.s@simformsolutions.com>
2023-03-31 14:58:12 +0530

    refactor: optimize code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { useEffect } from "react";
-import Dates from "./date";
-import Input from "./input";
-import Button from "./button";
+import Dates from "./componenets/date";
+import Input from "./componenets/input";
+import Button from "./componenets/button";
 
 import { useState } from "react";
 export default function App() {
-  
   // Date stored in localStorage and is checked so that one can get new empty list on another day
   useEffect(() => {
     const todayDate = new Date();
@@ -16,33 +15,34 @@ export default function App() {
     } else {
       //converted Date in string to object as todayDate is object and hence have to compare object with object
       const dateObject = new Date(dateInLocalStorage);
-//date comparision
+      //date comparision
       if (
-        dateObject.getFullYear() <= todayDate.getFullYear() &&
-        dateObject.getMonth() <= todayDate.getMonth() &&
-        dateObject.getDate() < todayDate.getDate()
+        dateObject.getFullYear() != todayDate.getFullYear() ||
+        dateObject.getMonth() != todayDate.getMonth() ||
+        dateObject.getDate() != todayDate.getDate()
       ) {
         localStorage.setItem("doneList", JSON.stringify([]));
         localStorage.setItem("itemList", JSON.stringify([]));
         localStorage.setItem("date", todayDate);
-        setItems([]);
-        setIsComplete([]);
+        if (items.length > 0) {
+          setItems([]);
+          setIsComplete([]);
+        }
       }
     }
   });
-// list store for a day and if started first at render than will initialize with empty array 
+  // list store for a day and if started first at render than will initialize with empty array
+  const getStoredList = JSON.parse(localStorage.getItem("itemList"));
   const [items, setItems] = useState(
-    JSON.parse(localStorage.getItem("itemList")) !== null
-      ? JSON.parse(localStorage.getItem("itemList"))
-      : []
+    getStoredList !== null ? getStoredList : []
   );
 
   const [shouldDisplay, setShouldDisplay] = useState(false);
-//set should siplay true for input field on clicking button
+  //set should siplay true for input field on clicking button
   function handleClick() {
     setShouldDisplay(true);
   }
-// enter key submission
+  // enter key submission
   function handleEnterKey(newTask) {
     setShouldDisplay(false);
     setItems([...items, newTask]);
@@ -54,24 +54,23 @@ export default function App() {
       alert("Task Added sucessfully");
     });
   }
-// escape key
+  // escape key
   function handleEscapeKey() {
     setShouldDisplay(false);
   }
-// storing the value of checkbox done or undone
+  // storing the value of checkbox done or undone
+  const getCheckedValues = JSON.parse(localStorage.getItem("doneList"));
   const [isComplete, setIsComplete] = useState(
-    JSON.parse(localStorage.getItem("doneList")) !== null
-      ? JSON.parse(localStorage.getItem("doneList"))
-      : []
+    getCheckedValues !== null ? getCheckedValues : []
   );
-  
+
   // setting event of checkbox in array
   function handleCheck(event, i) {
     const array = [...isComplete];
     array[i] = event.target.checked;
     setIsComplete(array);
     localStorage.setItem("doneList", JSON.stringify(array));
-    }
+  }
 
   return (
     <>
@@ -80,7 +79,7 @@ export default function App() {
           <Dates />
           <div className="input-list">
             <ul>
-              {items.map((itemvalue, i) => {
+              {items.map((itemValue, i) => {
                 return (
                   <div className="list d-flex" key={i}>
                     <div className="task-write">
@@ -89,14 +88,14 @@ export default function App() {
                           isComplete[i] == true ? "complete" : "incomplete"
                         }
                       >
-                        <li>{itemvalue}</li>
+                        <li>{itemValue}</li>
                       </h4>
                     </div>
                     <div className="round">
                       <input
                         type="checkbox"
                         id={`checkbox${i}`}
-                        // index to access particular checkbox 
+                        // index to access particular checkbox
                         checked={isComplete[i] == true ? true : false}
                         onChange={(event) => {
                           handleCheck(event, i);
--- src/componenets/date.js
+++ src/componenets/date.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+
 const day = [
   "SUNDAY",
   "MONDAY",
@@ -30,12 +30,12 @@ export default class Dates extends React.Component {
     return (
       <>
         <div className="time d-flex">
-          <div className="date_month_year d-flex">
+          <div className="date-month-year d-flex">
             <div className="date">
               <h1>{time.getDate()}</h1>
             </div>
 
-            <div className="month_year">
+            <div className="month-year">
               <div className="month">{month[time.getMonth()]}</div>
               <div className="year">{time.getFullYear()}</div>
             </div>
